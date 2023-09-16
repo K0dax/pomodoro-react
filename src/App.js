3205bc2acca4fe4fa00173e30eb9257bedfcf94c
Yan K0da Carlos <develyan7@gmail.com>
2023-09-16 19:30:22 -0300

    feat: countdown stop function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
 function padTime(time) {
@@ -9,13 +9,19 @@ export default function App() {
   const [title, setTitle] = useState('Let the countdown begin!');
   const [timeLeft, setTimeLeft] = useState(25 * 60);
 
+  const intervalRef = useRef(null);
+
   function startTimer() {
-    setInterval(() => {
+    intervalRef.current = setInterval(() => {
       setTimeLeft((timeLeft) => {
         if (timeLeft >= 1) return timeLeft - 1;
         return 0;
       });
     }, 1000);
+  }
+
+  function stopTimer() {
+    clearInterval(intervalRef.current);
   }
 
   const minutes = padTime(Math.floor(timeLeft / 60));
@@ -33,7 +39,7 @@ export default function App() {
 
       <div className="buttons">
         <button onClick={startTimer}>Start</button>
-        <button>Stop</button>
+        <button onClick={stopTimer}>Stop</button>
         <button>Reset</button>
       </div>
     </div>
